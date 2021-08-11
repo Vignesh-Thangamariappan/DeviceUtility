be6cbab09e1ed51ec2c04ddd7daef18f1f7b8338
Vignesh Thangamariappan <Vignesh.mariappan@full.co>
2021-08-11 12:34:41 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DeviceUtility
+
+A description of this package.
---
+++ Sources/DeviceUtility/DeviceUtility.swift
@@ -0,0 +1,257 @@
+import UIKit
+
+public enum DeviceModel: String {
+    
+    //Simulator
+    case simulator     = "simulator/sandbox",
+    
+    //iPod
+    iPod1              = "iPod 1",
+    iPod2              = "iPod 2",
+    iPod3              = "iPod 3",
+    iPod4              = "iPod 4",
+    iPod5              = "iPod 5",
+    
+    //iPad
+    iPad2              = "iPad 2",
+    iPad3              = "iPad 3",
+    iPad4              = "iPad 4",
+    iPad5              = "iPad 5", //iPad 2017
+    iPad6              = "iPad 6", //iPad 2018
+    iPad8              = "iPad 8",
+    
+    //iPad Mini
+    iPadMini           = "iPad Mini",
+    iPadMini2          = "iPad Mini 2",
+    iPadMini3          = "iPad Mini 3",
+    iPadMini4          = "iPad Mini 4",
+    iPadMini5          = "iPad Mini 5",
+    
+    //iPad Pro
+    iPadPro9_7         = "iPad Pro 9.7\"",
+    iPadPro10_5        = "iPad Pro 10.5\"",
+    iPadPro11          = "iPad Pro 11\"",
+    iPadPro12_9        = "iPad Pro 12.9\"",
+    iPadPro2_12_9      = "iPad Pro 2 12.9\"",
+    iPadPro3_12_9      = "iPad Pro 3 12.9\"",
+    iPadPro4_11        = "iPad Pro (11-inch) 4",
+    iPadPro4_12_9      = "iPad Pro (12.9-inch) 4",
+    
+    //iPad Air
+    iPadAir            = "iPad Air",
+    iPadAir2           = "iPad Air 2",
+    iPadAir3           = "iPad Air 3",
+    iPadAir4           = "iPad Air 4",
+    
+    
+    //iPhone
+    iPhone4            = "iPhone 4",
+    iPhone4S           = "iPhone 4S",
+    iPhone5            = "iPhone 5",
+    iPhone5S           = "iPhone 5S",
+    iPhone5C           = "iPhone 5C",
+    iPhone6            = "iPhone 6",
+    iPhone6Plus        = "iPhone 6 Plus",
+    iPhone6S           = "iPhone 6S",
+    iPhone6SPlus       = "iPhone 6S Plus",
+    iPhoneSE           = "iPhone SE",
+    iPhone7            = "iPhone 7",
+    iPhone7Plus        = "iPhone 7 Plus",
+    iPhone8            = "iPhone 8",
+    iPhone8Plus        = "iPhone 8 Plus",
+    iPhoneX            = "iPhone X",
+    iPhoneXS           = "iPhone XS",
+    iPhoneXSMax        = "iPhone XS Max",
+    iPhoneXR           = "iPhone XR",
+    iPhone11           = "iPhone 11",
+    iPhone11Pro        = "iPhone 11 Pro",
+    iPhone11ProMax     = "iPhone 11 Pro Max",
+    iPhone12Mini       = "iPhone 12 Mini",
+    iPhone12           = "iPhone 12",
+    iPhone12Pro        = "iPhone 12 Pro",
+    iPhone12ProMax     = "iPhone 12 Pro Max",
+    
+    //Apple TV
+    AppleTV            = "Apple TV",
+    AppleTV_4K         = "Apple TV 4K",
+    unrecognized       = "?unrecognized?"
+}
+
+public class DeviceUtility {
+    
+    public static let currentDevice = DeviceUtility()
+    
+    private init() {
+        
+    }
+    
+    public var modelIdentifier: String = {
+        
+        var size: Int = 0
+        sysctlbyname("hw.machine", nil, &size, nil, 0)
+        var machine = [CChar](repeating: 0,
+                              count: Int(size))
+        sysctlbyname("hw.machine", &machine, &size, nil, 0)
+        let deviceIdentifier: String = String(cString: machine)
+        return deviceIdentifier
+    }()
+    
+    public var model: DeviceModel {
+        
+        let models: [String: DeviceModel] = [
+            
+            //Simulator
+            "i386": .simulator,
+            "x86_64": .simulator,
+            
+            //iPod
+            "iPod1,1": .iPod1,
+            "iPod2,1": .iPod2,
+            "iPod3,1": .iPod3,
+            "iPod4,1": .iPod4,
+            "iPod5,1": .iPod5,
+            
+            //iPad
+            "iPad2,1": .iPad2,
+            "iPad2,2": .iPad2,
+            "iPad2,3": .iPad2,
+            "iPad2,4": .iPad2,
+            "iPad3,1": .iPad3,
+            "iPad3,2": .iPad3,
+            "iPad3,3": .iPad3,
+            "iPad3,4": .iPad4,
+            "iPad3,5": .iPad4,
+            "iPad3,6": .iPad4,
+            "iPad6,11": .iPad5, //iPad 2017
+            "iPad6,12": .iPad5,
+            "iPad7,5": .iPad6, //iPad 2018
+            "iPad7,6": .iPad6,
+            "iPad11,6": .iPad8,
+            "iPad11,7": .iPad8,
+            
+            //iPad Mini
+            "iPad2,5": .iPadMini,
+            "iPad2,6": .iPadMini,
+            "iPad2,7": .iPadMini,
+            "iPad4,4": .iPadMini2,
+            "iPad4,5": .iPadMini2,
+            "iPad4,6": .iPadMini2,
+            "iPad4,7": .iPadMini3,
+            "iPad4,8": .iPadMini3,
+            "iPad4,9": .iPadMini3,
+            "iPad5,1": .iPadMini4,
+            "iPad5,2": .iPadMini4,
+            "iPad11,1": .iPadMini5,
+            "iPad11,2": .iPadMini5,
+            
+            //iPad Pro
+            "iPad6,3": .iPadPro9_7,
+            "iPad6,4": .iPadPro9_7,
+            "iPad7,3": .iPadPro10_5,
+            "iPad7,4": .iPadPro10_5,
+            "iPad6,7": .iPadPro12_9,
+            "iPad6,8": .iPadPro12_9,
+            "iPad7,1": .iPadPro2_12_9,
+            "iPad7,2": .iPadPro2_12_9,
+            "iPad8,1": .iPadPro11,
+            "iPad8,2": .iPadPro11,
+            "iPad8,3": .iPadPro11,
+            "iPad8,4": .iPadPro11,
+            "iPad8,5": .iPadPro3_12_9,
+            "iPad8,6": .iPadPro3_12_9,
+            "iPad8,7": .iPadPro3_12_9,
+            "iPad8,8": .iPadPro3_12_9,
+            "iPad8,9": .iPadPro4_11,
+            "iPad8,10": .iPadPro4_11,
+            "iPad8,11": .iPadPro4_12_9,
+            "iPad8,12": .iPadPro4_12_9,
+            
+            //iPad Air
+            "iPad4,1": .iPadAir,
+            "iPad4,2": .iPadAir,
+            "iPad4,3": .iPadAir,
+            "iPad5,3": .iPadAir2,
+            "iPad5,4": .iPadAir2,
+            "iPad11,3": .iPadAir3,
+            "iPad11,4": .iPadAir3,
+            "iPad13,1": .iPadAir4,
+            "iPad13,2": .iPadAir4,
+            
+            //iPhone
+            "iPhone3,1": .iPhone4,
+            "iPhone3,2": .iPhone4,
+            "iPhone3,3": .iPhone4,
+            "iPhone4,1": .iPhone4S,
+            "iPhone5,1": .iPhone5,
+            "iPhone5,2": .iPhone5,
+            "iPhone5,3": .iPhone5C,
+            "iPhone5,4": .iPhone5C,
+            "iPhone6,1": .iPhone5S,
+            "iPhone6,2": .iPhone5S,
+            "iPhone7,1": .iPhone6Plus,
+            "iPhone7,2": .iPhone6,
+            "iPhone8,1": .iPhone6S,
+            "iPhone8,2": .iPhone6SPlus,
+            "iPhone8,4": .iPhoneSE,
+            "iPhone9,1": .iPhone7,
+            "iPhone9,3": .iPhone7,
+            "iPhone9,2": .iPhone7Plus,
+            "iPhone9,4": .iPhone7Plus,
+            "iPhone10,1": .iPhone8,
+            "iPhone10,4": .iPhone8,
+            "iPhone10,2": .iPhone8Plus,
+            "iPhone10,5": .iPhone8Plus,
+            "iPhone10,3": .iPhoneX,
+            "iPhone10,6": .iPhoneX,
+            "iPhone11,2": .iPhoneXS,
+            "iPhone11,4": .iPhoneXSMax,
+            "iPhone11,6": .iPhoneXSMax,
+            "iPhone11,8": .iPhoneXR,
+            "iPhone12,1": .iPhone11,
+            "iPhone12,3": .iPhone11Pro,
+            "iPhone12,5": .iPhone11ProMax,
+            "iPhone13,1": .iPhone12Mini,
+            "iPhone13,2": .iPhone12,
+            "iPhone13,3": .iPhone12Pro,
+            "iPhone13,4": .iPhone12ProMax,
+            
+            //Apple TV
+            "AppleTV5,3": .AppleTV,
+            "AppleTV6,2": .AppleTV_4K
+        ]
+        
+        guard let model = models[modelIdentifier] else {
+            return .unrecognized
+        }
+        
+        
+        if model == .simulator, let simModelCode = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"], let simModel = models[simModelCode] {
+            return simModel
+        } else {
+            return model
+        }
+    }
+}
+
+public extension DeviceUtility {
+    
+    private var notchDevices: [DeviceModel] {
+        [
+            .iPhoneX,
+            .iPhoneXR,
+            .iPhoneXS,
+            .iPhoneXSMax,
+            .iPhone11,
+            .iPhone11Pro,
+            .iPhone11ProMax,
+            .iPhone12,
+            .iPhone12Pro,
+            .iPhone12ProMax,
+            .iPhone12Mini
+        ]
+    }
+    
+    var hasNotch: Bool {
+        notchDevices.contains(model)
+    }
+}
