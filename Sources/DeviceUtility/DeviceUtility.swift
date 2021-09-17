a1e6ca73bf98e20255c75d5ca826c42329f7d2cb
Vignesh Thangamariappan <Vignesh.mariappan@full.co>
2021-09-17 10:57:39 +0530

    - Updated latest device models - Few refinements

--- Sources/DeviceUtility/DeviceUtility.swift
+++ Sources/DeviceUtility/DeviceUtility.swift
@@ -1,82 +1,5 @@
 import UIKit
 
-public enum DeviceModel: String {
-    
-    //Simulator
-    case simulator     = "simulator/sandbox",
-    
-    //iPod
-    iPod1              = "iPod 1",
-    iPod2              = "iPod 2",
-    iPod3              = "iPod 3",
-    iPod4              = "iPod 4",
-    iPod5              = "iPod 5",
-    
-    //iPad
-    iPad2              = "iPad 2",
-    iPad3              = "iPad 3",
-    iPad4              = "iPad 4",
-    iPad5              = "iPad 5", //iPad 2017
-    iPad6              = "iPad 6", //iPad 2018
-    iPad8              = "iPad 8",
-    
-    //iPad Mini
-    iPadMini           = "iPad Mini",
-    iPadMini2          = "iPad Mini 2",
-    iPadMini3          = "iPad Mini 3",
-    iPadMini4          = "iPad Mini 4",
-    iPadMini5          = "iPad Mini 5",
-    
-    //iPad Pro
-    iPadPro9_7         = "iPad Pro 9.7\"",
-    iPadPro10_5        = "iPad Pro 10.5\"",
-    iPadPro11          = "iPad Pro 11\"",
-    iPadPro12_9        = "iPad Pro 12.9\"",
-    iPadPro2_12_9      = "iPad Pro 2 12.9\"",
-    iPadPro3_12_9      = "iPad Pro 3 12.9\"",
-    iPadPro4_11        = "iPad Pro (11-inch) 4",
-    iPadPro4_12_9      = "iPad Pro (12.9-inch) 4",
-    
-    //iPad Air
-    iPadAir            = "iPad Air",
-    iPadAir2           = "iPad Air 2",
-    iPadAir3           = "iPad Air 3",
-    iPadAir4           = "iPad Air 4",
-    
-    
-    //iPhone
-    iPhone4            = "iPhone 4",
-    iPhone4S           = "iPhone 4S",
-    iPhone5            = "iPhone 5",
-    iPhone5S           = "iPhone 5S",
-    iPhone5C           = "iPhone 5C",
-    iPhone6            = "iPhone 6",
-    iPhone6Plus        = "iPhone 6 Plus",
-    iPhone6S           = "iPhone 6S",
-    iPhone6SPlus       = "iPhone 6S Plus",
-    iPhoneSE           = "iPhone SE",
-    iPhone7            = "iPhone 7",
-    iPhone7Plus        = "iPhone 7 Plus",
-    iPhone8            = "iPhone 8",
-    iPhone8Plus        = "iPhone 8 Plus",
-    iPhoneX            = "iPhone X",
-    iPhoneXS           = "iPhone XS",
-    iPhoneXSMax        = "iPhone XS Max",
-    iPhoneXR           = "iPhone XR",
-    iPhone11           = "iPhone 11",
-    iPhone11Pro        = "iPhone 11 Pro",
-    iPhone11ProMax     = "iPhone 11 Pro Max",
-    iPhone12Mini       = "iPhone 12 Mini",
-    iPhone12           = "iPhone 12",
-    iPhone12Pro        = "iPhone 12 Pro",
-    iPhone12ProMax     = "iPhone 12 Pro Max",
-    
-    //Apple TV
-    AppleTV            = "Apple TV",
-    AppleTV_4K         = "Apple TV 4K",
-    unrecognized       = "?unrecognized?"
-}
-
 public class DeviceUtility {
     
     public static let currentDevice = DeviceUtility()
@@ -214,6 +137,12 @@ public class DeviceUtility {
             "iPhone13,2": .iPhone12,
             "iPhone13,3": .iPhone12Pro,
             "iPhone13,4": .iPhone12ProMax,
+            "iPhone14,2": .iPhone13Pro,
+            "iPhone14,3": .iPhone13ProMax,
+            "iPhone14,4": .iPhone13Mini,
+            "iPhone14,5": .iPhone13,
+            "iPad12,2": .iPad9,
+            "iPad14,2": .iPadMini6,
             
             //Apple TV
             "AppleTV5,3": .AppleTV,
